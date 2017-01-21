b719a0aa9e6439d71faee2ab3727895cffef13bd
fabian20ro <fabian20ro@gmail.com>
2017-01-21 19:45:11 +0200

    add comment

--- src/test/java/scrabble/phrases/WordParserTest.java
+++ src/test/java/scrabble/phrases/WordParserTest.java
@@ -11,85 +11,97 @@ import org.junit.Test;
 
 import scrabble.phrases.filters.IWordFilter;
 
+/**
+ * The Class WordParserTest.
+ */
 public class WordParserTest {
 
+	private IWordFilter refuseAll = word -> false;
+	private IWordFilter wordsWithMoreThan10Chars = word -> word.getLength() > 10;
+
+	
+	/**
+	 * This test works but it takes too long to be run after each commit!.
+	 *
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
 	@Test
 	@Ignore
 	public void test() throws IOException {
-		
-		IWordFilter refuseAll = word -> false;
-		IWordFilter wordsWithMoreThan10Chars = word -> word.getLength() > 10;
-		
+
 		long t1 = System.currentTimeMillis();
-		
+
 		WordDictionary dictionary = new Main().getPopulatedDictionaryFromIncludedFile();
 
 		long t2 = System.currentTimeMillis();
-		
+
 		int accepted = dictionary.getTotalAcceptedWordCount();
 		int refused = dictionary.getTotalRefusedWordCount();
 		int unknown = dictionary.getTotalUnknownWordCount();
 		int total = dictionary.getTotalWordCount();
-		System.out.println("accepted: " + accepted + "\nrefused: " + refused + "\nunknown: " + unknown + "\ntotal: " + total);
+		System.out.println(
+				"accepted: " + accepted + "\nrefused: " + refused + "\nunknown: " + unknown + "\ntotal: " + total);
 		assertNotNull(dictionary.getRandomNoun());
 		assertNotNull(dictionary.getRandomAdjective());
 		assertNotNull(dictionary.getRandomVerb());
-		
-		
+
 		dictionary.addFilter(refuseAll);
 		assertEquals(accepted, dictionary.getTotalRefusedWordCount());
 		assertNull(dictionary.getRandomNoun());
 		assertNull(dictionary.getRandomAdjective());
 		assertNull(dictionary.getRandomVerb());
 		assertEquals(total, dictionary.getTotalWordCount());
-		
+
 		long t3 = System.currentTimeMillis();
-		
+
 		dictionary.removeFilter(refuseAll);
 		assertEquals(accepted, dictionary.getTotalAcceptedWordCount());
 		assertNotNull(dictionary.getRandomNoun());
 		assertNotNull(dictionary.getRandomAdjective());
 		assertNotNull(dictionary.getRandomVerb());
 		assertEquals(total, dictionary.getTotalWordCount());
-		
+
 		long t4 = System.currentTimeMillis();
-		
-		dictionary.clearFilters();
-		dictionary.addFilter(refuseAll); //TODO see why this second filter adition takes so much more compared to the first one
+
+		dictionary.addFilter(refuseAll); // TODO see why this second filter
+											// adition takes so much more
+											// compared to the first one
 		assertEquals(accepted, dictionary.getTotalRefusedWordCount());
 		assertNull(dictionary.getRandomNoun());
 		assertNull(dictionary.getRandomAdjective());
 		assertNull(dictionary.getRandomVerb());
 		assertEquals(total, dictionary.getTotalWordCount());
-		
+
 		long t5 = System.currentTimeMillis();
-		
+
 		dictionary.clearFilters();
 		assertNotNull(dictionary.getRandomNoun());
 		assertNotNull(dictionary.getRandomAdjective());
 		assertNotNull(dictionary.getRandomVerb());
 		assertEquals(total, dictionary.getTotalWordCount());
-		
+
 		long t6 = System.currentTimeMillis();
 
 		dictionary.addFilter(wordsWithMoreThan10Chars);
 		assertEquals(total, dictionary.getTotalWordCount());
 
 		long t7 = System.currentTimeMillis();
-		
+
 		dictionary.removeFilter(wordsWithMoreThan10Chars);
 		assertEquals(total, dictionary.getTotalWordCount());
 		assertEquals(accepted, dictionary.getTotalAcceptedWordCount());
-		
+
 		long t8 = System.currentTimeMillis();
-		
+
 		System.out.println("Initial dictionary population took: " + (t2 - t1) + " millis.");
 		System.out.println("Adding filter to remove them all took: " + (t3 - t2) + " millis.");
 		System.out.println("Removing filter to add them all back took: " + (t4 - t3) + " millis.");
 		System.out.println("Adding filter to remove them all again took: " + (t5 - t4) + " millis.");
 		System.out.println("Clearing all filters took: " + (t6 - t5) + " millis.");
 		System.out.println("Adding filter to remove all words with less than 11 characters: " + (t7 - t6) + " millis.");
-		System.out.println("Removing filter that removes all words with less than 11 characters: " + (t8 - t7) + " millis.");
+		System.out.println(
+				"Removing filter that removes all words with less than 11 characters: " + (t8 - t7) + " millis.");
 		System.out.println("Overall, it took: " + (t8 - t1) + " millis.");
 	}
 
