f1878adc686e4f0ea163831707db207d54b62227
fabian20ro <fabian20ro@gmail.com>
2017-01-23 06:49:15 +0200

    somewhat improve the syllable algorithm

--- src/main/java/scrabble/phrases/words/WordUtils.java
+++ src/main/java/scrabble/phrases/words/WordUtils.java
@@ -2,6 +2,7 @@ package scrabble.phrases.words;
 
 import java.util.HashMap;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class WordUtil.
  */
@@ -9,6 +10,10 @@ public class WordUtils {
 
 	/** The fixes. */
 	static HashMap<String, String> fixes = new HashMap<>();
+
+	/** Diftongi si triftongi. Nu am o idee mai buna. Neologisme nu merg (vezi testele). */
+	private static String[] tongs = new String[] { "iai", "eau", "iau", "oai", "ioa", "ia", "oa", "ea", "ua", "âu",
+			"ou", "ei", "ai", "oi", "ie", "ui" };
 
 	static {
 
@@ -21,6 +26,9 @@ public class WordUtils {
 		fixes.put("'", "");
 	}
 
+	/**
+	 * Instantiates a new word utils.
+	 */
 	private WordUtils() {
 
 	}
@@ -40,37 +48,94 @@ public class WordUtils {
 		return sentence;
 	}
 
+	// /**
+	// * Compute syllable number. Failed first try for some reason...
+	// *
+	// * @param word
+	// * the name
+	// * @return the int
+	// */
+	// public static int computeSyllableNumber(String word) {
+	// int result = 0;
+	// int length = word.length();
+	// boolean[] context = new boolean[length];
+	// for (int i = 0; i < length; i++) {
+	// context[i] = isVowel(word.charAt(i));
+	// }
+	// int currentChar = 0;
+	// while (currentChar < length) {
+	// if (context[currentChar]) {
+	// result++;
+	// if (currentChar + 1 < length && context[currentChar + 1]) {
+	// //next is also a vowel. we also include it
+	// if (currentChar >= 1 && currentChar + 2 < length && !context[currentChar
+	// - 1] && !context[currentChar + 2]) {
+	// //these two vowels are surrounded by consonants so it's actually two
+	// syllables, not one
+	// result++;
+	// currentChar++;
+	// }
+	// currentChar++;
+	// }
+	// }
+	// currentChar++;
+	// }
+	// return result;
+	// }
+
 	/**
 	 * Compute syllable number.
 	 *
 	 * @param word
-	 *            the name
+	 *            the word
 	 * @return the int
 	 */
 	public static int computeSyllableNumber(String word) {
 		int result = 0;
-		int length = word.length();
-		boolean[] context = new boolean[length];
-		for (int i = 0; i < length; i++) {
-			context[i] = isVowel(word.charAt(i));
-		}
-		int currentChar = 0;
-		while (currentChar < length) {
-			if (context[currentChar]) {
+		char[] chars = word.toCharArray();
+		replaceTongsWithChar(chars);
+		for (int i =0; i < chars.length; i++) {
+			if (isVowel(chars[i])) {
 				result++;
-				if (currentChar + 1 < length && context[currentChar + 1]) {
-					//next is also a vowel. we also include it
-					if (currentChar >= 1 && currentChar + 2 < length && !context[currentChar - 1] && !context[currentChar + 2]) {
-						//these two vowels are surrounded by consonants so it's actually two syllables, not one
-						result++;
-						currentChar++;
-					}
-					currentChar++;
-				}
 			}
-			currentChar++;
 		}
 		return result;
+	}
+
+	/**
+	 * Replace tongs.
+	 *
+	 * @param chars
+	 *            the chars
+	 */
+	private static void replaceTongsWithChar(char[] chars) {
+		int clength = chars.length;
+		for (String tong : tongs) {
+			char[] tch = tong.toCharArray();
+			for (int i = 0; i < clength - tong.length() + 1; i++) {
+				if (tch.length == 3) {
+					if (tch[0] == chars[i] && tch[1] == chars[i + 1] && tch[2] == chars[i + 2]) {
+						chars[i] = 'z';
+						chars[i + 2] = 'z';
+						i += 2;
+					}
+				} else if (tch[0] == chars[i] && tch[1] == chars[i + 1]) {
+					// diftong sau hiat? se pare ca hiat-ul e mai rar...
+					if (i > 0) {
+						if (!isVowel(chars[i - 1])) {
+							chars[i + 1] = 'z';
+						} else if (isVowel(chars[i - 1])) {
+							chars[i] = 'z';
+						}
+						i++;
+					}
+				}
+			}
+		}
+		if (clength >= 2 && isVowel(chars[clength - 1]) && isVowel(chars[clength - 2])
+				&& chars[clength - 1] != chars[clength - 2]) {
+			chars[clength - 2] = 'z';
+		}
 	}
 
 	/**
@@ -84,6 +149,13 @@ public class WordUtils {
 		return name.substring(Math.max(0, name.length() - 3));
 	}
 
+	/**
+	 * Checks if is vowel.
+	 *
+	 * @param c
+	 *            the c
+	 * @return true, if is vowel
+	 */
 	private static boolean isVowel(char c) {
 		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'ă' || c == 'â' || c == 'î';
 	}
--- src/test/java/scrabble/phrases/words/WordUtilsTest.java
+++ src/test/java/scrabble/phrases/words/WordUtilsTest.java
@@ -18,9 +18,14 @@ public class WordUtilsTest {
 	@Test
 	public void testSyllables() {
 
-		Map<String, Integer> wordMap = m("piuitoare 5", "semifinalista 6", "greoi 2");
-		// wordMap.putAll(m("piuitor 4", "miercureana 4", "policioara 4",
-		// "vulpoaica 3", "miau 1", "leoaica 3", "lupoaica 3"));
+		//see https://dexonline.ro/articol/5.2._Despărțirea_în_interiorul_cuvintelor
+		//for full epicness: 'Normele actuale prevăd despărțirea după pronunțare.'
+		Map<String, Integer> wordMap = m();
+		wordMap.putAll(m("semifinalista 6", "greoi 2", "aalenian 4", "alee 3", "alcool 3", "fiinta 3",
+				"puicuta 3", "aeroport 4", "miercureana 4", "policioara 4", "vulpoaica 3", "miau 1", "leoaica 3",
+				"lupoaica 3", "mioara 2", "ambiguul 4", "tămâie 3", "bou 1", "reusit 3", "greul 2", "plouat 2",
+				"roua 2", "calea 2", "eu 1", "greu 1", "pui 1", "tuiul 2", "ghioc 2"));
+		// not working: "bour 2", "spleen 1", "piui 3", "piuitoare 5", "piuitor 4", "boreal 3"
 		wordMap.putAll(m());
 
 		for (String word : wordMap.keySet()) {
