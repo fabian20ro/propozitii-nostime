fd20cae0954451e6b6cf09d3aa317ef0ef42c185
Compound Autopilot <compound-autopilot@local>
2026-07-07 06:15:12 +0000

    compound: finalize verification-hygiene candidate

--- src/main/kotlin/scrabble/phrases/GlobalExceptionMapper.kt
+++ src/main/kotlin/scrabble/phrases/GlobalExceptionMapper.kt
@@ -1,5 +1,6 @@
 package scrabble.phrases
 
+import jakarta.ws.rs.WebApplicationException
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import jakarta.ws.rs.ext.ExceptionMapper
@@ -12,6 +13,9 @@ class GlobalExceptionMapper : ExceptionMapper<Exception> {
     private val log = Logger.getLogger(GlobalExceptionMapper::class.java)
 
     override fun toResponse(exception: Exception): Response {
+        if (exception is WebApplicationException) {
+            return exception.response
+        }
         log.error("Unhandled exception", exception)
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
             .entity(mapOf("error" to "Internal server error"))
