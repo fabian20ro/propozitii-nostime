47a66f57885d0222f3a8ddf39409b2f77ed0d0e2
Compound Autopilot <compound-autopilot@local>
2026-07-07 17:10:53 +0000

    compound: finalize tiny-debug candidate

--- src/main/kotlin/scrabble/phrases/decorators/HtmlVerseBreaker.kt
+++ src/main/kotlin/scrabble/phrases/decorators/HtmlVerseBreaker.kt
@@ -4,7 +4,7 @@ import scrabble.phrases.providers.ISentenceProvider
 
 class HtmlVerseBreaker(private val provider: ISentenceProvider) : ISentenceProvider {
     override fun getSentence(): String {
-        val text = provider.getSentence()
+        val text = provider.getSentence() ?: return ""
         return text.replace(Regex("(<[^>]*>)|(\\s*/\\s*)")) {
             if (it.value.startsWith("<")) {
                 it.value
