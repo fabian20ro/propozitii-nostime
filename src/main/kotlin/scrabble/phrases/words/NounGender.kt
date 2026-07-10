46aa5c7c7062e114817f2e1cc29a5d63fd531862
Compound Autopilot <compound-autopilot@local>
2026-07-10 00:44:32 +0000

    compound: finalize verification-hygiene candidate

--- src/main/kotlin/scrabble/phrases/words/NounGender.kt
+++ src/main/kotlin/scrabble/phrases/words/NounGender.kt
@@ -1,5 +1,18 @@
 package scrabble.phrases.words
 
 enum class NounGender {
-    M, F, N
+    M, F, N;
+
+    companion object {
+        fun fromCode(code: String?): NounGender? {
+            if (code.isNullOrEmpty()) return null
+            return try {
+                valueOf(code.uppercase())
+            } catch (_: IllegalArgumentException) {
+                null
+            }
+        }
+
+        fun toCode(gender: NounGender): String = gender.name.first().toString()
+    }
 }
